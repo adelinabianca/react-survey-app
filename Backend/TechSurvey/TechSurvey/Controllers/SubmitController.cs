c857e58c600c047c5f71becaa54613ae595cc788
Mihaela Radulescu <m.radulescu@levi9.com>
2018-10-31 15:45:58 +0200

    Initial commit.

---
+++ Backend/TechSurvey/TechSurvey/Controllers/SubmitController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Http;
+using TechSurvey.Models;
+
+namespace TechSurvey.Controllers
+{
+    public class SubmitController : ApiController
+    {
+        //test method
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            return Ok("OK");
+        }
+
+        [HttpPost]
+        public IHttpActionResult Post([FromBody]SurveyData surveyData)
+        {
+            return Ok(surveyData);
+        }
+    }
+}
