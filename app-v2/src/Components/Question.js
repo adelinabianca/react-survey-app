c7c6f3c8085592976ee432d2a1cbe49c9b890488
Adelina Bianca Dascalu <a.dascalu@levi9.com>
2018-11-01 16:41:31 +0200

    added save button and http requests(commented)

--- app-v2/src/Components/Question.js
+++ app-v2/src/Components/Question.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 import { CheckboxList } from './CheckboxList';
 import { RadioButtonList } from './RadioButtonList';
 import { Textarea } from './Textarea';
@@ -39,36 +41,74 @@ export class Question extends Component {
     onSubmit = () => {
         const { saveQuestionAnswer } = this.props;
         saveQuestionAnswer(this.state.questionAnswers);
+        // post method
+        const answersToSave = this.props.answers.filter(prevAnswer => prevAnswer.questionId !== this.state.questionAnswers.questionId).concat(this.state.questionAnswers)
+        const url = window.location.href;
+        const startChar = url.indexOf(':', 8);
+        const userId = url.substr(startChar + 1, url.length);
+        const finalAnswers = {
+            userId: userId,
+            answers: answersToSave
+        };
+        // console.log(JSON.stringify(finalAnswers))
+        // axios.post(urlForPOSTAnswers, JSON.stringify(finalAnswers)).then(response => console.log(response));
+        this.props.goToNextQuestion();
     }
-  render() {
-    const {question} = this.props;
-    const isDisabled = !((this.props.question.id === this.state.questionAnswers.questionId && this.state.questionAnswers.answers.length !== 0) || 
+
+
+    showComponent() {
+        if(this.props.questionIndex <= this.props.lastQuestionIndex) {
+            const {question} = this.props;
+            const isDisabled = !((this.props.question.id === this.state.questionAnswers.questionId && this.state.questionAnswers.answers.length !== 0) || 
                         (this.props.previousAnswer !== ''  && this.props.question.id !== this.state.questionAnswers.questionId  && this.props.previousAnswer.answers.length !== 0));
+            return (
+                <div className="question">
+                    <h2 className="pb-3 pt-3 mbr-fonts-style display-2">
+                        {this.props.question.question}
+                    </h2>
+                    <h3 className="mbr-section-subtitle align-center mbr-light mbr-fonts-style display-7 pb-2 pt-2 pl-2 pr-2 mb-4">
+                        {this.props.question.description}
+                    </h3>
+                    {this.showAnswerOptions(question.answerType, question.answerOptions)}
+                    <div className="media-container-row buttonPadding">
+                    <Button buttonText='Previous'
+                            className={'previousAction btn-prev ' + (this.props.questionIndex === 0 ? 'hidden' : '')}
+                            icon='fa-chevron-left'
+                            onClick={this.onPreviousButtonClicked} />
+                    <Button className={'nextAction ' + (this.props.questionIndex === this.props.lastQuestionIndex ? 'hidden' : '')}
+                            buttonText='Save'
+                            disabled={isDisabled}
+                            icon='fa-share-square'
+                            onClick={this.onSubmit} />
+                    <Button className={'nextAction btn-next ' + (this.props.questionIndex === this.props.lastQuestionIndex ? 'hidden' : '')}
+                            buttonText='Next'
+                            disabled={isDisabled}
+                            icon='fa-chevron-right'
+                            onClick={this.onNextButtonClicked} />
+                    <Button className={'submitAction btn-next ' + (!(this.props.questionIndex === this.props.lastQuestionIndex) ? 'hidden' : '')}
+                            buttonText='Submit'
+                            disabled={isDisabled}
+                            icon='fa-share-square'
+                            onClick={this.onSubmit} />
+                    </div>
+                </div>
+            )
+        } else {
+            return (
+                <div style={{textAlign:'center'}}>
+                    <h3>Thank you for submitting!</h3>
+                </div>
+            )
+        }
+    }
+
+  render() {
+    // const {question} = this.props;
+    // const isDisabled = !((this.props.question.id === this.state.questionAnswers.questionId && this.state.questionAnswers.answers.length !== 0) || 
+    //                     (this.props.previousAnswer !== ''  && this.props.question.id !== this.state.questionAnswers.questionId  && this.props.previousAnswer.answers.length !== 0));
     return (
-        <div className="question">
-            <h2 className="pb-3 pt-3 mbr-fonts-style display-2">
-                {this.props.question.question}
-            </h2>
-            <h3 className="mbr-section-subtitle align-center mbr-light mbr-fonts-style display-7 pb-2 pt-2 pl-2 pr-2 mb-4">
-                {this.props.question.description}
-            </h3>
-            {this.showAnswerOptions(question.answerType, question.answerOptions)}
-            <div className="media-container-row buttonPadding">
-            <Button buttonText='Previous'
-                    className={'previousAction btn-prev ' + (this.props.questionIndex === 0 ? 'hidden' : '')}
-                    icon='fa-chevron-left'
-                    onClick={this.onPreviousButtonClicked} />
-            <Button className={'nextAction btn-next ' + (this.props.questionIndex === this.props.lastQuestionIndex ? 'hidden' : '')}
-                    buttonText='Next'
-                    disabled={isDisabled}
-                    icon='fa-chevron-right'
-                    onClick={this.onNextButtonClicked} />
-            <Button className={'submitAction btn-next ' + (!(this.props.questionIndex === this.props.lastQuestionIndex) ? 'hidden' : '')}
-                    buttonText='Submit'
-                    disabled={isDisabled}
-                    icon='fa-share-square'
-                    onClick={this.onSubmit} />
-            </div>
+        <div>
+            {this.showComponent()}
         </div>
     )
   }
