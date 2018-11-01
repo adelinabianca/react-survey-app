340bb97bd200266094e86d7a7e3a388886a2a525
Andrei Diaconu <adiaconu@travix.com>
2018-11-01 23:10:36 +0200

    Find empty row dinamically add EF and TechSurveyAnswers.xlsx

--- Backend/TechSurvey/TechSurvey/Services/ExcelService.cs
+++ Backend/TechSurvey/TechSurvey/Services/ExcelService.cs
@@ -6,32 +6,44 @@ namespace TechSurvey.Services
 {
     public class ExcelService
     {
-        private readonly string excelFilePath = @"C:\Users\a.diaconu\OneDrive - Levi9 IT Services\TestExcelCharts.xlsx";
+        private readonly string excelFilePath =
+            @"C:\Users\a.diaconu\OneDrive - Levi9 IT Services\TechSurveyAnswers.xlsx";
 
         public void UpdateExcel(SurveyData surveyData)
         {
             //using Interop.Excel
             Application excelApp = new Application();
             var workbooks = excelApp.Workbooks;
-            var openedWorkbook = workbooks.Open(excelFilePath, 0, false, 5, "", "", true, XlPlatform.xlWindows, "\t", true, false, 0, true, 1, 0);
+            var openedWorkbook = workbooks.Open(excelFilePath, 0, false, 5, "", "", true, XlPlatform.xlWindows, "\t",
+                true, false, 0, true, 1, 0);
             var allWorksheets = openedWorkbook.Worksheets;
-            Worksheet firstWorksheet = (Worksheet)allWorksheets.get_Item(1);
+            Worksheet firstWorksheet = (Worksheet) allWorksheets.get_Item(1);
             Range worksheetCells = firstWorksheet.Cells;
 
-            var cellWithCounterOfRows = (Range)worksheetCells.get_Item(1, 9);
-            int row = (int)cellWithCounterOfRows.Value2;
-            row++;
-            cellWithCounterOfRows.Value = row;
+            //find row based on value row value saved in excel
+            /*            var cellWithCounterOfRows = (Range) worksheetCells.get_Item(1, 9);
+                        int row = (int) cellWithCounterOfRows.Value2;
+                        row++;
+                        cellWithCounterOfRows.Value = row;*/
+            //dynamically find first empty row
+            int rowIdx = 0;
+            int notEmpty = 1;
+            while (notEmpty > 0)
+            {
+                string aCellAddress = "A" + (++rowIdx);
+                Range row = excelApp.get_Range(aCellAddress, aCellAddress).EntireRow;
+                notEmpty = (int) excelApp.WorksheetFunction.CountA(row);
+            }
 
             //update actual data
             var columnIndex = 1;
-            var cellGuid = (Range)worksheetCells.Item[row, columnIndex];
+            var cellGuid = (Range) worksheetCells.Item[rowIdx, columnIndex];
             cellGuid.Value = surveyData.UserId;
 
             foreach (var answer in surveyData.Answers)
             {
                 columnIndex++;
-                var cellToUpdate = (Range)worksheetCells.Item[row, columnIndex];
+                var cellToUpdate = (Range) worksheetCells.Item[rowIdx, columnIndex];
                 cellToUpdate.Value = answer.AnswerValue;
             }
 
@@ -39,7 +51,7 @@ namespace TechSurvey.Services
             int worksheetsCount = allWorksheets.Count;
             for (int iWorksheet = 1; iWorksheet <= worksheetsCount; iWorksheet++)
             {
-                Worksheet ws = (Worksheet)allWorksheets.get_Item(iWorksheet);
+                Worksheet ws = (Worksheet) allWorksheets.get_Item(iWorksheet);
                 var usedRange = ws.UsedRange;
                 usedRange.Formula = usedRange.Formula;
                 ws.Calculate();
