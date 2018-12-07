4a97549c1787c436a8bdf1514f36073970f16aa1
Lucian Ailenei <luciancostin.ailenei@gmail.com>
2018-12-07 23:15:31 +0200

    V2 dashboards

--- Realtime/routes/questionnaire.js
+++ Realtime/routes/questionnaire.js
@@ -2,10 +2,8 @@ var express = require('express');
 var router = express.Router();
 const io = require('../socket').connection;
 
-router.post('/questionnaire', function(req, res) {
-  console.log(req.body); 
-
-  req.app.io.emit('message', "lalaalla");
+router.post('/questionnaire', function(req, res) { 
+  req.app.io.emit('questionnaire', { questionnaire: req.body });
 
   res.send("ACK");
 });
