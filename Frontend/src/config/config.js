bac06b69e697c51e74458cbf42f8f41988d79f16
Adelina Bianca Dascalu <a.dascalu@levi9.com>
2018-12-07 20:18:04 +0200

    added second submit request

--- Frontend/src/config/config.js
+++ Frontend/src/config/config.js
@@ -1,2 +1,3 @@
 export const GET_SURVEY_URL = "http://localhost:64282/api/survey?uid=";
 export const SUBMIT_SURVEY_URL = "http://localhost:64282/api/Submit";
+export const SUBMIT_REAL_TIME_URL = "";
