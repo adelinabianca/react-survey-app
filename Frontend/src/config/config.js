377dd2e75a0bfaff729028a117ee3f3871e5ed7b
Andrei Diaconu <andreidiaconu90@gmail.com>
2018-12-08 09:29:00 +0200

    Fix survey submit endpoint

--- Frontend/src/config/config.js
+++ Frontend/src/config/config.js
@@ -1,4 +1,4 @@
 export const GET_SURVEY_URL = "https://survey-hack9.azurewebsites.net/Survey?uid=";
-export const SUBMIT_SURVEY_URL = "https://survey-hack9.azurewebsites.net/Submit";
+export const SUBMIT_SURVEY_URL = "https://survey-hack9.azurewebsites.net/Survey";
 export const SUBMIT_REAL_TIME_URL = "https://survey-dashboards.azurewebsites.net/questionnaire/";
 export const DEMO_URL = "https://survey-hack9.azurewebsites.net/Feedback?formName="
