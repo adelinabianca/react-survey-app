5065caf3e9b81b49464f4db9cf3be232f19e968a
Lucian Ailenei <luciancostin.ailenei@gmail.com>
2018-12-08 13:04:47 +0200

    final commit

--- Realtime/routes/questionnaire.js
+++ Realtime/routes/questionnaire.js
@@ -3,6 +3,7 @@ var router = express.Router();
 const io = require('../socket').connection;
 
 router.post('/questionnaire', function(req, res) { 
+  console.log(req.body);
   req.app.io.emit('questionnaireStatus', { questionnaireStatus: req.body });
 
   res.send("ACK");
