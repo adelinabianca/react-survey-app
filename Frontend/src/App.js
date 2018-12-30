ecdefed26eb3fd3dfecbd3f2c6115fc7713a74f0
Andrei Diaconu <andreidiaconu90@gmail.com>
2018-12-30 18:54:09 +0200

    Change title

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -243,11 +243,11 @@ class App extends Component {
                 <div className="col-md-12">
                   <div className="mbr-white">
                     <h1 className="mbr-section-title align-center mbr-bold pb-3 pt-3 mbr-fonts-style display-1">
-                      LEVININE TECH SURVEY 2018
+                      THE STATE OF LEVININE DEVELOPER ECOSYSTEM
                     </h1>
-                    <p className="mbr-text align-center pb-3 mbr-fonts-style display-5">
-					 THE STATE OF LEVININE DEVELOPER ECOSYSTEM IN 2018
-                    </p>
+                    // <p className="mbr-text align-center pb-3 mbr-fonts-style display-5">
+					 // THE STATE OF LEVININE DEVELOPER ECOSYSTEM IN 2018
+                    // </p>
                   </div>
                 </div>
               </div>
